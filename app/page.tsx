3e13f03fc0a1f66d966840cf48fd0f956bab7695
Huxley <29369728+HuxleyMc@users.noreply.github.com>
2023-07-31 12:27:23 +1000

    feat: basic landing page with docs

--- app/page.tsx
+++ app/page.tsx
@@ -1,23 +1,166 @@
+import {
+  allTrendingResponse,
+  developersTrendingResponse,
+  languageTrendingResponse,
+} from "@/constants/exampleResponses";
 import Image from "next/image";
+
+const Endpoint = ({
+  endpoint,
+  description,
+  params,
+  response,
+  exampleUrl,
+}: {
+  endpoint: string;
+  exampleUrl?: string;
+  description: string;
+  params: Array<{
+    name: string;
+    description: string;
+    required: boolean;
+    possibleValues?: Array<string>;
+  }>;
+  response: string;
+}) => {
+  return (
+    <div className="mb-8">
+      <h3 className="text-xl font-bold mt-6 mb-2">{description}</h3>
+      <p className={`${exampleUrl ? "mb-2" : "mb-4"} font-semibold`}>
+        Endpoint: <code className="p-1 font-normal">{endpoint}</code>
+      </p>
+      {exampleUrl ? (
+        <p className="mb-4 font-semibold">
+          Example URL: <code className="p-1 font-normal">{exampleUrl}</code>
+        </p>
+      ) : null}
+      <p className="mb-2 font-bold">Parameters:</p>
+      <ul className="list-disc list-inside mb-4">
+        {params.map((param) => (
+          <ul key={param.name}>
+            <p>
+              <span className="font-medium">
+                {param.required ? "Required:" : "Optional:"}
+              </span>{" "}
+              {param.description}
+            </p>
+            <code className="p-1">`{param.name}`</code>:{" "}
+            {param.possibleValues && (
+              <>
+                {param.possibleValues.map((value) => (
+                  <code key={value} className=" p-1">
+                    {value}
+                  </code>
+                ))}
+              </>
+            )}
+          </ul>
+        ))}
+      </ul>
+      <p className="mb-4 font-bold">Response Format:</p>
+      <pre className="p-2 border-white border bg-slate-200 bg-opacity-10">
+        {response}
+      </pre>
+    </div>
+  );
+};
 
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <p className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0">
-            Made By Huxley
-          </p>
-        </div>
-      </div>
+      <div className="container mx-auto p-6">
+        <h1 className="text-4xl font-bold mb-6">GitHub Trending API</h1>
+        <p className="mb-4">
+          This repository provides an API to fetch trending repositories and
+          developers from GitHub. The API is hosted at the base URL:
+          <code className="font-semibold p-1">
+            `https://gh-trending.vercel.app`
+          </code>
+        </p>
 
-      <div className="mb-32 grid text-center lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <div className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30">
-          <h2 className={`mb-3 text-2xl font-semibold`}>Docs</h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </div>
+        <h2 className="text-2xl font-bold mt-8 mb-4">API Endpoints</h2>
+
+        <Endpoint
+          endpoint="/api/trending"
+          description="Get trending repositories."
+          params={[
+            {
+              name: "since",
+              description: "Filter repositories by trending duration.",
+              required: false,
+              possibleValues: ["daily", "weekly", "monthly"],
+            },
+            {
+              name: "spoken_language_code",
+              description: "Filter repositories by spoken language code.",
+              required: false,
+              possibleValues: ["en,", "jp,", "etc..."],
+            },
+          ]}
+          response={allTrendingResponse}
+        />
+
+        <Endpoint
+          endpoint="/api/trending/[language]"
+          exampleUrl="/api/trending/swift"
+          description="Get trending repositories by language."
+          params={[
+            {
+              name: "since",
+              description: "Filter repositories by trending duration.",
+              required: false,
+              possibleValues: ["daily", "weekly", "monthly"],
+            },
+            {
+              name: "spoken_language_code",
+              description: "Filter repositories by spoken language code.",
+              required: false,
+              possibleValues: ["en,", "jp,", "etc..."],
+            },
+          ]}
+          response={languageTrendingResponse}
+        />
+
+        <Endpoint
+          endpoint="/api/trending/developers"
+          description="Get trending developers."
+          params={[
+            {
+              name: "since",
+              description: "Filter developers by trending duration.",
+              required: false,
+              possibleValues: ["daily", "weekly", "monthly"],
+            },
+            {
+              name: "sponsorable",
+              description: "Filter developers who are sponsorable.",
+              required: false,
+              possibleValues: ["1"],
+            },
+          ]}
+          response={developersTrendingResponse}
+        />
+
+        <Endpoint
+          endpoint="/api/trending/developers/[language]"
+          exampleUrl="/api/trending/developers/typescript"
+          description="Get trending developers by language."
+          params={[
+            {
+              name: "since",
+              description: "Filter developers by trending duration.",
+              required: false,
+              possibleValues: ["daily", "weekly", "monthly"],
+            },
+            {
+              name: "sponsorable",
+              description: "Filter developers who are sponsorable.",
+              required: false,
+              possibleValues: ["1"],
+            },
+          ]}
+          response={developersTrendingResponse}
+        />
       </div>
     </main>
   );
---
+++ constants/exampleResponses.ts
@@ -0,0 +1,67 @@
+export const allTrendingResponse = `[
+  {
+    author: "huggingface",
+    avatar: "https://github.com/huggingface.png",
+    builtBy: [
+      {
+        avatar: "https://avatars.githubusercontent.com/u/23298448?s=40&v=4",
+        url: "https://github.com/OlivierDehaene",
+        username: "OlivierDehaene",
+      },
+      ...
+    ],
+    canSponsor: false,
+    description: "Large Language Model Text Generation Inference",
+    forks: 366,
+    language: "Python",
+    languageColor: "#3572A5",
+    name: "text-generation-inference",
+    newStars: 74,
+    stars: 3684,
+    url: "https://github.com/huggingface/text-generation-inference",
+  },
+  ...
+]`;
+
+export const languageTrendingResponse = `[
+  {
+    author: "groue",
+    avatar: "https://github.com/groue.png",
+    builtBy: [
+      {
+        avatar: "https://avatars.githubusercontent.com/u/54219?s=40&v=4",
+        url: "https://github.com/groue",
+        username: "groue",
+      },
+      ...
+    ],
+    canSponsor: true,
+    description:
+      "A toolkit for SQLite databases, with a focus on application development",
+    forks: 643,
+    language: "Swift",
+    languageColor: "#F05138",
+    name: "GRDB.swift",
+    newStars: 5,
+    stars: 6051,
+    url: "https://github.com/groue/GRDB.swift",
+  },
+  ...
+]`;
+
+export const developersTrendingResponse = `[
+  {
+    name: "Steven Tey",
+    nickname: "steven-tey",
+    avatar: "https://avatars.githubusercontent.com/u/28986134?s=96&v=4",
+    url: "https://github.com/steven-tey",
+    popularRepository: {
+      name: "dub",
+      description:
+        "An open-source link shortener with built-in analytics + free custom domains.",
+      url: "https://github.com/steven-tey/dub",
+    },
+    canSponsor: false,
+  },
+  ...
+]`;
